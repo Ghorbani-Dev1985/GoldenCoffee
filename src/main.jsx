1a438af17ae9984719328f31dbbefc05b3b83330
mGhorbani1985 <mohamad.ghorbanii1985@gmail.com>
2023-08-25 15:23:17 +0330

    add context

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './Context/UsersContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <UserProvider>
     <App />
+  </UserProvider>
   </BrowserRouter>,
 )
