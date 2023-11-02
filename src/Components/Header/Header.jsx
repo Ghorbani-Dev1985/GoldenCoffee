f47910b119833c3244ddd25b39632ff1c5cd7cd9
mGhorbani1985 <mohamad.ghorbanii1985@gmail.com>
2023-11-02 18:32:53 +0330

    feat : develop header component and dark mode toggle

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,120 +1,133 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { HiOutlineShoppingCart, HiOutlineMoon, HiOutlineSun  } from "react-icons/hi";
+import {
+  HiOutlineShoppingCart,
+  HiOutlineMoon,
+  HiOutlineSun,
+} from "react-icons/hi";
 import { BiLogIn } from "react-icons/bi";
-
+import DarkMode from "../DarkMode/DarkMode";
 
 const CoffeeSubMenuItems = [
   {
     id: 1,
-    to: "",
+    to: "/login",
     title: "قهوه ویژه",
   },
   {
     id: 2,
-    to: "",
+    to: "/login2",
     title: "ویژه در سطح جهانی",
   },
   {
     id: 3,
-    to: "",
+    to: "/login3",
     title: "قهوه درجه یک",
   },
   {
     id: 4,
-    to: "",
+    to: "/login4",
     title: " ترکیبات تجاری ",
   },
   {
     id: 5,
-    to: "",
+    to: "/logi5",
     title: " ترکیبات تجاری",
   },
   {
     id: 6,
-    to: "",
+    to: "/logi6",
     title: " قهوه زینو برزیلی",
   },
 ];
 
+
 const Header = () => {
-const [themeToggle , setThemeToddle] = useState(false);
   return (
     <header className="w-90p h-24 flex items-center bg-black/50 backdrop-blur-[6px] mx-auto px-10 py-5 rounded-3xl fixed top-9 right-0 left-0 z-50">
-     <div className="w-full flex justify-between items-center">
-         {/* Logo, Menu */}
-      <nav className="flex items-center gap-x-9">
-        <div>
-          <img src="../../src/assets/Images/app-logo.png" alt="Golden Coffee" />
-        </div>
-        <ul className="flex gap-x-9 text-xl text-gray-300 tracking-tightest">
-          <MenuItem to="/" title="صفحه اصلی" className="text-orange-200"/>
-          <MenuItem to="/login" title="فروشگاه">
-            <ul className="hidden">
-              {CoffeeSubMenuItems.map(({ id, to, title }) => {
-                return (
-                  <li key={id} className="font-DanaMd">
+      <div className="w-full flex justify-between items-center">
+        {/* Logo, Menu */}
+        <nav className="flex items-center gap-x-9 h-14">
+          <div>
+            <img
+              src="../../src/assets/Images/app-logo.png"
+              alt="Golden Coffee"
+            />
+          </div>
+          <ul className="flex gap-x-9 h-full text-xl text-gray-300 tracking-tightest child:leading-[56px]">
+            <MenuItem to="/" title="صفحه اصلی" className="text-orange-200" />
+            <MenuItem to="/login" title="فروشگاه" className="relative group">
+              <div
+                className="absolute top-full opacity-0 invisible group-hover:opacity-100 group-hover:visible w-52 p-6 space-y-4 bg-white dark:bg-zinc-700 shadow-normal text-zinc-700 dark:text-white font-Dana text-base
+               tracking-normal border-t-[3px] border-orange-300 rounded-2xl transition-all child:inline-block child:transition-colors child-hover:text-orange-300"
+              >
+                {CoffeeSubMenuItems.map(({ id, to, title }) => {
+                  return (
                     <NavLink
+                      key={id}
                       to={to}
                       className={({ isActive }) =>
                         isActive
-                          ? "font-bold text-orange-200"
-                          : "text-gray-300"
+                          ? "transition-colors text-orange-200"
+                          : "text-zinc-700 dark:text-white"
                       }
                     >
                       {title}
                     </NavLink>
-                  </li>
-                );
-              })}
-            </ul>
-          </MenuItem>
-          <MenuItem to="/login2" title="دیکشنری" />
-          <MenuItem to="/login3" title="بلاک" />
-          <MenuItem to="/login5" title="درباره‌ ما" />
-          <MenuItem to="/login6" title="تماس‌ با‌ ما" />
-        </ul>
-      </nav>
-      {/* Cart, ThemeToggle, LoginBtn */}
-      <section className="flex gap-x-10 text-orange-200 text-xl font-normal">
-             <div className="flex items-center gap-x-5">
-                {/* Cart */}
-                <div>
-                <HiOutlineShoppingCart className="w-8 h-8" />
-                </div>
-                {/* ThemeToggle */}
-             <button onClick={()=> setThemeToddle(!themeToggle)}>
-                {
-                    themeToggle ? <HiOutlineSun className="w-8 h-8" /> : <HiOutlineMoon className="w-8 h-8" />
-                }
-             </button>
+                  );
+                })}
               </div>
-              {/* DivideBorder */}
-              <span className="h-14 w-px bg-white/20 block"></span>
-              {/* LoginLink */}
-             <Link className="flex items-center gap-x-2.5 tracking-tightest"><BiLogIn className="w-8 h-8" />
-               ورود |‌ ثبت نام
-             </Link>
-      </section>
-     </div>
+            </MenuItem>
+            <MenuItem to="/login7" title="دیکشنری" />
+            <MenuItem to="/login3" title="بلاک" />
+            <MenuItem to="/login5" title="درباره‌ ما" />
+            <MenuItem to="/login6" title="تماس‌ با‌ ما" />
+          </ul>
+        </nav>
+        {/* Cart, ThemeToggle, LoginBtn */}
+        <section className="flex gap-x-10 text-orange-200 text-xl font-normal">
+          <div className="flex items-center gap-x-5">
+            {/* Cart */}
+            <div>
+              <div className="py-3">
+                <HiOutlineShoppingCart className="w-8 h-8" />
+                {/* Cart Box */}
+                <div></div>
+              </div>
+            </div>
+            {/* ThemeToggle */}
+            <DarkMode />
+          </div>
+          {/* DivideBorder */}
+          <span className="h-14 w-px bg-white/20 block"></span>
+          {/* LoginLink */}
+          <Link className="flex items-center gap-x-2.5 tracking-tightest">
+            <BiLogIn className="w-8 h-8" />
+            ورود |‌ ثبت نام
+          </Link>
+        </section>
+      </div>
     </header>
   );
 };
 
 export default Header;
 
-const MenuItem = ({ to, title, children }) => {
+const MenuItem = ({ to, title, children, className }) => {
   return (
-    <li className="font-DanaMd">
-        <NavLink
+    <li className={`font-DanaMd ${className}`}>
+      <NavLink
         to={to}
         className={({ isActive }) =>
           isActive
             ? "text-orange-200"
-            : "text-gray-300"
+            : "text-gray-300 transition-colors hover:text-orange-300"
         }
       >
-        {title}
+        <span className="transition-colors group-hover:text-orange-300">
+          {" "}
+          {title}
+        </span>
       </NavLink>
       {children}
     </li>
